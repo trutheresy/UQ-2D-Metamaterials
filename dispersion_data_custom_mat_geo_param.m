479417159e3efb6a09eca92431518db05274137a
Han Zhang <zhang.han@alumni.ubc.ca>
2023-07-27 14:04:05 -0700

    10000 mc samples run on 30x30 geometry

--- dispersion_data_custom_mat_geo_param.m
+++ dispersion_data_custom_mat_geo_param.m
@@ -13,8 +13,8 @@ function [EIGENVALUE_DATA, WAVEVECTOR_DATA] = dispersion_data_custom_mat_geo_par
     %%Default Constants
     const.a = 1; % [m]
     const.N_ele = 1;
-    const.N_pix = 10; % Assumes square matrix
-    %const.N_pix = geo_rows; % Assumes square matrix
+    %const.N_pix = 10; % Assumes square matrix
+    const.N_pix = geo_rows; % Assumes square matrix
     const.N_wv = [11 NaN]; const.N_wv(2) = ceil(const.N_wv(1)/2); % used for full IBZ calculations
     const.N_k = 50; % used for IBZ contour calculations
     const.N_eig = 4;
