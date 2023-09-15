51b4c8f3e9854d064b0a42d4fd580238d49b2268
Han Zhang <zhang.han@alumni.ubc.ca>
2023-09-15 16:16:36 -0400

    gamma studies script

--- Run_Matlab.sh
+++ Run_Matlab.sh
@@ -1,3 +1,3 @@
 #PBS -N Han_script
 cd $PBS_O_WORKDIR || exit 1
-/usr/local/MATLAB/R2022a/bin/matlab -nodisplay -nodesktop -batch han_script_geometry_material_properties
+/shared/Apps/MATLAB/R2023a/bin/matlab -nodisplay -nodesktop -batch han_script_geometry_material_properties
